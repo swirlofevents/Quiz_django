2cf0afc0a4c8a621237f9e88d9515115891346b3
Nikita Kalashnikov <swirlofevents@gmail.com>
2019-05-06 15:35:55 +0500

    second commit

--- quest/models.py
+++ quest/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class AnswersModel(models.Model):
 
     answer = models.TextField(max_length=400, verbose_name="Ответ")
+    votes = models.CharField(max_length=4096, verbose_name="кол-во голосов")
 
 #    def __str__(self):
 #        return str(self.answer)
